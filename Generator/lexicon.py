6356e4ccff9c31376fe92e3a7a6e59bcc5b5af57
Shuvam Vidyarthy <127395812+SkredX@users.noreply.github.com>
2025-12-16 19:53:09 +0530

    Commit lexicon.py

---
+++ Generator/lexicon.py
@@ -0,0 +1,28 @@
+SUBJECTS = [
+    "I", "We", "They", "He", "She"
+]
+
+OBJECTS = [
+    "this movie",
+    "the food",
+    "the service",
+    "the product",
+    "the experience"
+]
+
+POSITIVE_VERBS = [
+    "like", "love", "enjoy"
+]
+
+NEGATIVE_VERBS = [
+    "hate", "dislike", "regret"
+]
+
+ADVERBS = {
+    "": 1.0,
+    "really": 1.2,
+    "very": 1.3,
+    "absolutely": 1.5,
+    "hardly": -0.7,
+    "barely": -0.6
+}
