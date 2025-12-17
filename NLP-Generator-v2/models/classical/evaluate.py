361a4246586ca595cb028d9bb206ba4287b05605
Shuvam Vidyarthy <127395812+SkredX@users.noreply.github.com>
2025-12-17 20:40:46 +0530

    Update evaluate.py

--- NLP-Generator-v2/models/classical/evaluate.py
+++ NLP-Generator-v2/models/classical/evaluate.py
@@ -1,1 +1,10 @@
+from sklearn.metrics import accuracy_score, classification_report
 
+def evaluate(model, X_test_vec, y_test):
+    preds = model.predict(X_test_vec)
+
+    print("Accuracy:", accuracy_score(y_test, preds))
+    print()
+    print(classification_report(y_test, preds))
+
+    return preds
