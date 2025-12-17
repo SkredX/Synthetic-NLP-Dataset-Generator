ad4db2e208bad88e6add0dfa6b7a80ac4a0ea8b5
Shuvam Vidyarthy <127395812+SkredX@users.noreply.github.com>
2025-12-17 20:35:23 +0530

    Update rules.py

--- NLP-Generator-v2/generator/grammar/rules.py
+++ NLP-Generator-v2/generator/grammar/rules.py
@@ -1,1 +1,53 @@
+class GrammarRules:
+    
+    # Polarity:
+        # +1 : Affirmative
+        # -1 : Negative
+        # 0 : Interrogative
 
+    def __init__(self, vocab):
+        self.vocab = vocab
+
+        # Core lexical groups (must exist in tokenizer)
+        self.subjects = ["i", "you", "he", "she", "we", "they"]
+        self.verbs_positive = ["like", "love", "enjoy"]
+        self.verbs_aux = ["do", "does", "did", "is", "are", "was", "were"]
+        self.negators = ["not"]
+        self.objects = ["this", "that", "it"]
+        self.question_words = ["what", "why", "how"]
+
+        self._validate_vocab()
+
+    def _validate_vocab(self):
+        
+        all_tokens = (
+            self.subjects
+            + self.verbs_positive
+            + self.verbs_aux
+            + self.negators
+            + self.objects
+            + self.question_words
+            + ["?"]
+        )
+
+        missing = [t for t in all_tokens if t not in self.vocab.word2id]
+        if missing:
+            raise ValueError(f"Missing tokens in vocabulary: {missing}")
+
+
+
+    def affirmative(self, subject, verb, obj):
+        
+        return [subject, verb, obj], 1
+
+    def negative(self, subject, aux, verb, obj):
+        
+        return [subject, aux, "not", verb, obj], -1
+
+    def interrogative_aux(self, aux, subject, verb, obj):
+       
+        return [aux, subject, verb, obj, "?"], 0
+
+    def interrogative_wh(self, wh_word, aux, subject, verb):
+        
+        return [wh_word, aux, subject, verb, "?"], 0
